14b7b0691c315d61bc6d9fb1bff21d1f1d0d87a5
killerDJO <killerDJO@gmail.com>
2018-02-03 15:28:32 +0200

    cleanup tasks

--- src/main/RendererLocationProvider.ts
+++ src/main/RendererLocationProvider.ts
@@ -2,8 +2,6 @@ import * as path from "path";
 
 export class RendererLocationProvider {
     public static getRendererLocation(): string {
-        return process.env.NODE_ENV === "dev"
-            ? "http://localhost:8080"
-            : `file:${path.resolve(__dirname, "..\\renderer\\index.html")}`;
+        return `file:${path.resolve(__dirname, "..\\renderer\\index.html")}`;
     }
 }
