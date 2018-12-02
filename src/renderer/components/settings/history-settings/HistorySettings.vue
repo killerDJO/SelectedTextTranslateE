09b55b0a14b15e3aff5407dde4ba16171e6adb2e
killerDJO <killerDJO@gmail.com>
2018-12-02 20:48:44 +0200

    ui fixes

--- src/renderer/components/settings/history-settings/HistorySettings.vue
+++ src/renderer/components/settings/history-settings/HistorySettings.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <div class="form-group inline-form-group">
-      <label>Number of records per page:</label>
-      <input v-tab-index type="number" min="1" class="form-control" v-model.number.lazy="numberOfRecordsPerPage">
-    </div>
-    <div class="settings-separator"/>
     <checkbox v-model="currentHistorySettings.isContinuousSyncEnabled" :label="'Enable continuous history sync'" />
     <div class="form-group inline-form-group" v-if="currentHistorySettings.isContinuousSyncEnabled">
       <label>Pull interval (minutes):</label>
       <input v-tab-index type="number" min="1" class="form-control" v-model.number.lazy="syncIntervalMinutes">
+    </div>
+    <div class="settings-separator"/>
+    <div class="form-group inline-form-group">
+      <label>Number of records per page:</label>
+      <input v-tab-index type="number" min="1" class="form-control" v-model.number.lazy="numberOfRecordsPerPage">
     </div>
     <div class="settings-separator"/>
     <checkbox v-model="currentHistorySettings.backupOnApplicationStart" :label="'Backup history database on application start'"/>
