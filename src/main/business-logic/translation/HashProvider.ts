d42ffe4b037be73da6184c8f95434267a4241efe
killerDJO <killerDJO@gmail.com>
2019-01-07 16:30:32 +0200

    added hash description

--- src/main/business-logic/translation/HashProvider.ts
+++ src/main/business-logic/translation/HashProvider.ts
@@ -17,31 +17,32 @@ export class HashProvider {
         );
     }
 
+    // Jenkins hash: https://en.wikipedia.org/wiki/Jenkins_hash_function
     private computeHashWithConfig(text: string, translationConfig: TranslationConfig): string {
         const bytes = Buffer.from(text, "utf8");
         let a: number = translationConfig.tkk1;
-        const pow32: number = 4294967295;
-        const pow31: number = (pow32 - 1) / 2;
+        const pow32minus1: number = 4294967295;
+        const pow31minus1: number = (pow32minus1 - 1) / 2;
 
         for (let i = 0; i < bytes.length; ++i) {
             a = a + bytes[i];
 
             /* ------ */
-            a = a + ((a << 10) & pow32);
+            a = a + ((a << 10) & pow32minus1);
             a = a ^ (a >>> 6);
             /* ------ */
         }
 
         /* ------ */
-        a = a + ((a << 3) & pow32);
+        a = a + ((a << 3) & pow32minus1);
         a = a ^ (a >>> 11);
-        a = a + ((a << 15) & pow32);
+        a = a + ((a << 15) & pow32minus1);
         /* ------ */
 
         a = a ^ translationConfig.tkk2;
 
         if (a < 0) {
-            a = (a & pow31) + pow31 + 1;
+            a = (a & pow31minus1) + pow31minus1 + 1;
         }
 
         a = a % 1000000;
