ffc2611fc36cc3907558da25a1b0a337a8a1a78d
killerDJO <killerDJO@gmail.com>
2019-02-16 13:44:43 +0200

    updated dependencies

--- src/main/utils/catch-error-with-empty-result.ts
+++
@@ -1,9 +0,0 @@
-import { empty, OperatorFunction } from "rxjs";
-import { catchError } from "rxjs/operators";
-
-export function catchErrorWithEmptyResult<TResult>(callback: (error: Error) => void): OperatorFunction<TResult, TResult> {
-    return catchError<TResult, TResult>(error => {
-        callback(error);
-        return empty();
-    });
-}
