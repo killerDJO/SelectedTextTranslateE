d8bcfc1d6521430060b75a5136238536eed77edb
yevhenii.andrushchak <>
2023-01-05 01:24:16 +0200

    pack & install

--- build-scripts/create-installer.js
+++ build-scripts/create-installer.js
@@ -1,31 +1,32 @@
-const electronInstaller = require("electron-winstaller");
-const path = require("path");
-const del = require("del");
+const electronInstaller = require('electron-winstaller');
+const path = require('path');
+const rimraf = require('rimraf');
 
-const platformAndArch = `${process.platform}-${process.arch}`
+const platformAndArch = `${process.platform}-${process.arch}`;
 
 const installerConfig = {
-    appDirectory: path.join(__dirname, `../dist/package/Selected Text Translate-${platformAndArch}`),
-    outputDirectory: path.join(__dirname, "../dist/installer"),
-    exe: "Selected Text Translate.exe",
-    title: `Selected Text Translate Setup`,
-    setupExe: `Selected Text Translate Setup.exe`,
-    iconUrl: path.join(__dirname, "../dist/app/main/icons/tray.ico"),
-    noMsi: true
+  appDirectory: path.join(__dirname, `../dist/package/Selected Text Translate-${platformAndArch}`),
+  outputDirectory: path.join(__dirname, '../dist/installer'),
+  exe: 'Selected Text Translate.exe',
+  title: `Selected Text Translate Setup`,
+  setupExe: `Selected Text Translate Setup.exe`,
+  iconUrl: path.join(__dirname, '../dist/app/main/presentation/icons/tray.ico'),
+  noMsi: true
 };
 
 clean();
 createInstaller();
 
 function createInstaller() {
-    resultPromise = electronInstaller.createWindowsInstaller(installerConfig);
+  resultPromise = electronInstaller.createWindowsInstaller(installerConfig);
 
-    resultPromise.then(
-        () => console.log("Installer has been created."),
-        (e) => console.log(`Error creating installer: ${e.message}`));
+  resultPromise.then(
+    () => console.log('Installer has been created.'),
+    e => console.log(`Error creating installer: ${e.message}`)
+  );
 }
 
 function clean() {
-    del.sync([installerConfig.outputDirectory]);
-    console.log(`\n${installerConfig.outputDirectory} has been cleaned.\n`)
+  rimraf.sync(installerConfig.outputDirectory, { recursive: true, force: true });
+  console.log(`\n${installerConfig.outputDirectory} has been cleaned.\n`);
 }
--- build-scripts/package.js
+++ build-scripts/package.js
@@ -1,21 +1,24 @@
 const packager = require('electron-packager');
 const path = require('path');
-const del = require('del');
+const rimraf = require('rimraf');
 const fs = require('fs');
-const _ = require('lodash-es');
+const _ = require('lodash');
 
 const packageConfig = {
   arch: 'x64',
   asar: true,
   dir: path.join(__dirname, '../'),
-  icon: path.join(__dirname, '../dist/app/main/icons/tray.ico'),
-  ignore: pathsFilter,
-  afterCopy: [modifySettings],
+  icon: path.join(__dirname, '../dist/app/main/presentation/icons/tray.ico'),
+  ignore: ignorePath,
+  afterCopy: [modifySettings, copyCommonPackage],
   executableName: 'Selected Text Translate',
   out: path.join(__dirname, '../dist/package'),
   overwrite: true,
-  platform: 'win32'
+  platform: 'win32',
+  prune: true
 };
+
+const INTERNAL_MODULE_NAME = '@selected-text-translate';
 
 clean();
 package();
@@ -26,18 +29,17 @@ function package() {
       console.log(`App has been packaged.`);
     })
     .catch(reason => {
-      console.log(`\n${console.log('`electron-packager`')} says...\n`);
+      console.log(`\nelectron-packager says...\n`);
       console.log(reason + '\n');
     });
 }
 
 function clean() {
-  console.log(packageConfig.out);
-  del.sync([packageConfig.out]);
+  rimraf.sync(packageConfig.out, { recursive: true, force: true });
   console.log(`\n${packageConfig.out} has been cleaned.\n`);
 }
 
-function pathsFilter(currentPath) {
+function ignorePath(currentPath) {
   const whiteListedPaths = ['', '/dist', '/package.json'];
   for (const whiteListedPath of whiteListedPaths) {
     if (currentPath === whiteListedPath) {
@@ -45,14 +47,7 @@ function pathsFilter(currentPath) {
     }
   }
 
-  const blacklistedExtensions = ['.gitkeep'];
-  for (const blacklistedExtension of blacklistedExtensions) {
-    if (currentPath.endsWith(blacklistedExtension)) {
-      return true;
-    }
-  }
-
-  return !currentPath.startsWith('/dist/app');
+  return !currentPath.startsWith('/dist/app') && !currentPath.startsWith('/node_modules');
 }
 
 function modifySettings(buildPath, electronVersion, platform, arch, callback) {
@@ -63,4 +58,44 @@ function modifySettings(buildPath, electronVersion, platform, arch, callback) {
   const mergedSettings = _.extend(defaultSettings, productionSettings);
   fs.writeFileSync(defaultSettingsPath, JSON.stringify(mergedSettings, null, 4));
   callback();
+}
+
+function copyCommonPackage(buildPath, electronVersion, platform, arch, callback) {
+  const outputFolder = path.join(buildPath, `node_modules/${INTERNAL_MODULE_NAME}`);
+  const sourceFolder = path.join(__dirname, '..', 'packages/common');
+  copyFolderRecursiveSync(sourceFolder, outputFolder);
+  callback();
+}
+
+function copyFileSync(source, target) {
+  var targetFile = target;
+
+  if (fs.existsSync(target)) {
+    if (fs.lstatSync(target).isDirectory()) {
+      targetFile = path.join(target, path.basename(source));
+    }
+  }
+
+  fs.writeFileSync(targetFile, fs.readFileSync(source));
+}
+
+function copyFolderRecursiveSync(source, target) {
+  var files = [];
+
+  var targetFolder = path.join(target, path.basename(source));
+  if (!fs.existsSync(targetFolder)) {
+    fs.mkdirSync(targetFolder);
+  }
+
+  if (fs.lstatSync(source).isDirectory()) {
+    files = fs.readdirSync(source);
+    files.forEach(function (file) {
+      var curSource = path.join(source, file);
+      if (fs.lstatSync(curSource).isDirectory()) {
+        copyFolderRecursiveSync(curSource, targetFolder);
+      } else {
+        copyFileSync(curSource, targetFolder);
+      }
+    });
+  }
 }
